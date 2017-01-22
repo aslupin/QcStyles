4e2452d1a960a20ffc709b25d8a5a006b29fa072
aslupin <poon_arsene_lupin@hotmail.com>
2017-01-22 18:23:54 +0700

    Initial commit

---
+++ Qc Styles v.0.1/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(404, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(31, 336);
+            this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Action",
+            "School",
+            "Shoujo",
+            "Drama",
+            "None"});
+            this.comboBox1.Location = new System.Drawing.Point(277, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "ZDefault - BOT",
+            "ZDefault - TOP",
+            "ZDefault - italics",
+            "ZDefault - flashback"});
+            this.comboBox3.Location = new System.Drawing.Point(277, 115);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(118, 21);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(236, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Link";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(320, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 132);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(227, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Genres :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(224, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Storage :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(48, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Current script :";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Location = new System.Drawing.Point(129, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, -2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "x";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, -6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "_";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.ErrorImage = global::WindowsFormsApplication1.Properties.Resources.P1;
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.P11;
+            this.pictureBox1.InitialImage = global::WindowsFormsApplication1.Properties.Resources.P1;
+            this.pictureBox1.Location = new System.Drawing.Point(232, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 59);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 188);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(424, 188);
+            this.Name = "Form1";
+            this.Opacity = 0.97D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QcStyles Utility Version 0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Qc Styles v.0.1/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string FileAss;
+        string TextForUse;
+        string StyleZix;
+        string NameStyle;
+        string[] TextSave;
+        string TEXTD;
+        string LogFile;
+        List<string> _items = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+           
+        }
+        // Function for find word
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // Browse File .ass (Subtitle)
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Open File Dialog";
+            fdlg.InitialDirectory = LogFile;//@Logfile
+            LogFile = fdlg.FileName;
+            fdlg.Filter = "All Files (*.*)|*.*|ASS Files (*.ass)|*.ass";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+               
+                FileAss = fdlg.FileName;
+
+
+                // Input Style in list
+                string[] Words = File.ReadAllLines(FileAss);
+                int NumList = 0;
+                for (int i = 0; i < Words.Length; i++)
+                {
+                    Words[i] = Words[i].Substring(0, Words[i].IndexOf(",") + 1);
+                    string s1 = Words[i];
+                    string s2 = "Style: ";
+                    string s3 = "WrapStyle: ";
+                    bool R, C;
+                    R = s1.Contains(s2);
+                    C = s1.Contains(s3);
+                    if (R == true && C == false)
+                    {
+                        // MessageBox.Show(Words[i]);
+                        Words[i] = Words[i].Replace("Style: ", "");
+                        TextForUse = Words[i].Replace(",", "");
+                        _items.Add(TextForUse);
+                        NumList++;
+                        listBox1.DataSource = null;
+                        listBox1.DataSource = _items;
+                    }
+
+
+                }
+
+                String[] lines2;
+                lines2 = File.ReadAllLines(FileAss);
+                for (int i = 0; i < lines2.Length; i++)
+                {
+                    TEXTD = TEXTD + lines2[i] + Environment.NewLine;
+                }
+            }
+
+          
+
+
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Filelog = LogFile;
+        
+        }
+     
+       
+        // Select Styles in file
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NameStyle = listBox1.GetItemText(listBox1.SelectedItem);
+     
+        }
+        // Select Resolution
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+  
+        }
+        // Select Genres
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Select Storage
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        // Link
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Process For Change
+          
+
+
+            // ACTION
+         if (comboBox1.SelectedItem == "Action") {
+
+                if (comboBox3.SelectedItem == "ZDefault - BOT")
+                {
+                    StyleZix = ",DilleniaUPC,75,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,2,100,100,25,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - TOP")
+                {
+                    StyleZix = ",DilleniaUPC,75,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,8,100,100,15,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - italics")
+                {
+                    StyleZix = ",DilleniaUPC,75,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,-1,0,0,100,100,0,0,1,2,0,2,100,100,15,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - flashback")
+                {
+                    StyleZix = ",DilleniaUPC,75,&H00FFFFFF,&H000000FF,&H006E6E6E,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,2,100,100,15,1";
+                }
+         }
+                // SCHOOL
+             if (comboBox1.SelectedItem == "School") {
+
+                if (comboBox3.SelectedItem == "ZDefault - BOT")
+                {
+                    StyleZix = ",Layiji MaHaNiYom V1.5 OT,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,0,0,0,100,100,0,0,1,2.4,0.4,2,0,0,35,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - TOP")
+                {
+                    StyleZix = ",Layiji MaHaNiYom V1.5 OT,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,0,0,0,100,100,0,0,1,2.4,0.4,8,0,0,15,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - italics")
+                {
+                    StyleZix = ",Layiji MaHaNiYom V1.5 OT,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,-1,0,0,100,100,0,0,1,2.4,0.4,2,0,0,35,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - flashback")
+                {
+                    StyleZix = ",Layiji MaHaNiYom V1.5 OT,70,&H00FFFFFF,&H000000FF,&H006E6E6E,&H00000000,0,0,0,0,100,100,0,0,1,2.4,0.4,2,0,0,35,1";
+                }}
+
+
+                 // SHOUJO
+                 if (comboBox1.SelectedItem == "Shoujo") {
+
+                if (comboBox3.SelectedItem == "ZDefault - BOT")
+                {
+                    StyleZix = ",Layiji MaHaNiYom BAO 1.2,75,&H00F1F4F9,&H000019FF,&H00000000,&HB4000000,-1,0,0,0,100,100,0,0,1,2.4,0.8,2,80,80,30,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - TOP")
+                {
+                    StyleZix = ",Layiji MaHaNiYom BAO 1.2,75,&H00F1F4F9,&H000019FF,&H00000000,&HB4000000,-1,0,0,0,100,100,0,0,1,2.4,0.8,8,80,80,15,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - italics")
+                {
+                    StyleZix = ",Layiji MaHaNiYom BAO 1.2,75,&H00F1F4F9,&H000019FF,&H00000000,&HB4000000,-1,-1,0,0,100,100,0,0,1,2.4,0.8,2,80,80,30,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - flashback")
+                {
+                    StyleZix = ",Layiji MaHaNiYom BAO 1.2,75,&H00F1F4F9,&H000019FF,&H006E6E6E,&HB4000000,-1,0,0,0,100,100,0,0,1,2.4,0.8,2,80,80,30,1";
+                }
+                 }
+                    
+                     // DRAMA
+                if (comboBox1.SelectedItem == "Drama")
+                {
+
+                    if (comboBox3.SelectedItem == "ZDefault - BOT")
+                    {
+                        StyleZix = ",2005_iannnnnMTV,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,0,0,0,105,107,0,0,1,2.7,0.5,2,0,0,35,1";
+                    }
+                    if (comboBox3.SelectedItem == "ZDefault - TOP")
+                    {
+                        StyleZix = ",2005_iannnnnMTV,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,0,0,0,105,107,0,0,1,2.7,0.5,8,0,0,16,1";
+                    }
+                    if (comboBox3.SelectedItem == "ZDefault - italics")
+                    {
+                        StyleZix = ",2005_iannnnnMTV,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,-1,0,0,105,107,0,0,1,2.7,0.5,2,0,0,35,1";
+                    }
+                    if (comboBox3.SelectedItem == "ZDefault - flashback")
+                    {
+                        StyleZix = ",2005_iannnnnMTV,70,&H00FFFFFF,&H000000FF,&H006E6E6E,&H00000000,0,0,0,0,105,107,0,0,1,2.7,0.5,2,0,0,35,1";
+                    }
+                }
+
+                         // NONE
+                if (comboBox1.SelectedItem == "None") {
+
+                if (comboBox3.SelectedItem == "ZDefault - BOT")
+                {
+                    StyleZix = ",TH Niramit AS,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,-1,0,0,0,107,105,0,0,1,2.7,0.5,2,0,0,35,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - TOP")
+                {
+                    StyleZix = ",TH Niramit AS,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,-1,0,0,0,107,105,0,0,1,2.7,0.5,8,0,0,16,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - italics")
+                {
+                    StyleZix = ",TH Niramit AS,70,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,-1,-1,0,0,107,105,0,0,1,2.7,0.5,2,0,0,35,1";
+                }
+                if (comboBox3.SelectedItem == "ZDefault - flashback")
+                {
+                    StyleZix = ",TH Niramit AS,70,&H00FFFFFF,&H000000FF,&H006E6E6E,&H00000000,-1,0,0,0,107,105,0,0,1,2.7,0.5,2,0,0,35,1";
+                }}
+               
+        
+            
+
+
+           /* List<string> found = new List<string>();
+            string line;
+            using (StreamReader file = new StreamReader(FileAss))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("NameStyle"))
+                    {
+                       // found.Add(line);
+                       // Filed = line;
+                    }
+                }
+            }*/
+
+            // Change text
+            //MessageBox.Show(NameStyle);
+               // String Linetest = FileDialog();
+               String[] lines;
+             lines = File.ReadAllLines(FileAss);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string s1 = lines[i];
+                string s2 = "Style: " + NameStyle + ",";
+                bool b0;
+                bool b;
+                b0 = TEXTD.Contains(s2);
+                b = s1.Contains(s2);
+                if (b == true)
+                {
+                   // MessageBox.Show(lines[i]);
+                   // MessageBox.Show(TEXTD.Replace(lines[i], "Style: " + NameStyle + StyleZix));
+                   TEXTD =  TEXTD.Replace(lines[i], "Style: " + NameStyle + StyleZix);
+                   // MessageBox.Show(s2);
+                  //  lines[i] = "Style: " + NameStyle + StyleZix;
+                   // MessageBox.Show(lines[i]);
+                }
+              /*  if (b == true && b0 == true)
+                {
+                    TEXTD.Replace(s2, lines[i]);
+                }*/
+               // if(i==0)
+               // TEXTD = TEXTD + lines[i] + Environment.NewLine;
+              //  if(i!=0)
+
+              
+
+            }
+           
+           
+        //   TextSave = lines;
+         //  MessageBox.Show(TEXTD);
+          
+                 //  File.WriteAllLines(TEXTD, lines);
+        }
+        // Save as
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "ASS Files (*.ass)|*.ass";
+            saveFileDialog1.Title = "Save as";
+            saveFileDialog1.ShowDialog();
+
+
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+          
+
+                fs.Close();
+            }
+            string name = saveFileDialog1.FileName;
+            TEXTD = TEXTD.Replace(",0,0,0,,", "::Qc.Styles.01.ByAsLupin,0,0,0,,");
+            File.WriteAllText(name, TEXTD);
+         //   MessageBox.Show(TEXTD);
+        
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LogFile = Properties.Settings.Default.Filelog;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+      
+        
+             }}
+
+    
